cf3fdc97d44097e3e54d753352959298b6473b6d
RushitSoni <115700646+RushitSoni@users.noreply.github.com>
2023-04-08 09:35:35 +0530

    Assignment

---
+++ WebApplication1/WebApplication1/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Department
+    {
+
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string? Name { get; set; }
+
+    }
+}
