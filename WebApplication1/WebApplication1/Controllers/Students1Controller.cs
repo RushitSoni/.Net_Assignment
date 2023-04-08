d05011003e70c66c83ac7c42e5b87e12ec2bf550
RushitSoni <115700646+RushitSoni@users.noreply.github.com>
2023-04-08 15:13:08 +0530

    change1

---
+++ WebApplication1/WebApplication1/Controllers/Students1Controller.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class Students1Controller : Controller
+    {
+        private readonly MVCDBContext _context;
+
+        public Students1Controller(MVCDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Students1
+        public async Task<IActionResult> Index( string searchString)
+        {
+            
+            ViewData["CurrentFilter"] = searchString;
+
+
+            var dbContext_Demo = _context.Students.Include(s => s.department);
+            var students = from s in dbContext_Demo
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.department.Name.Contains(searchString));
+
+            }
+
+
+            return View(await students.AsNoTracking().ToListAsync());
+
+        }
+
+        // GET: Students1/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.department)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students1/Create
+        public IActionResult Create()
+        {
+            ViewData["DeptId"] = new SelectList(_context.Departments, "ID", "Name");
+            return View();
+        }
+
+        // POST: Students1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Class,DeptId")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DeptId"] = new SelectList(_context.Departments, "ID", "Name", student.DeptId);
+            return View(student);
+        }
+
+        // GET: Students1/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            ViewData["DeptId"] = new SelectList(_context.Departments, "ID", "Name", student.DeptId);
+            return View(student);
+        }
+
+        // POST: Students1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Class,DeptId")] Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DeptId"] = new SelectList(_context.Departments, "ID", "Name", student.DeptId);
+            return View(student);
+        }
+
+        // GET: Students1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.department)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Students == null)
+            {
+                return Problem("Entity set 'MVCDBContext.Students'  is null.");
+            }
+            var student = await _context.Students.FindAsync(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+          return (_context.Students?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
